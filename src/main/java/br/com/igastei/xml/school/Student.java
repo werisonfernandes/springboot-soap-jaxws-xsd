ce685b2e86225e6d7f4144e4d406a5df0a7985b9
Werison Aparecido Fernandes <waparecf@everis.com>
2021-06-18 17:33:29 -0300

    Initial commit

---
+++ src/main/java/br/com/igastei/xml/school/Student.java
@@ -0,0 +1,116 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2021.06.18 às 05:28:04 PM BRT 
+//
+
+
+package br.com.igastei.xml.school;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java de Student complex type.
+ * 
+ * <p>O seguinte fragmento do esquema especifica o conteúdo esperado contido dentro desta classe.
+ * 
+ * <pre>
+ * &lt;complexType name="Student">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="standard" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="address" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Student", propOrder = {
+    "name",
+    "standard",
+    "address"
+})
+public class Student {
+
+    @XmlElement(required = true)
+    protected String name;
+    protected int standard;
+    @XmlElement(required = true)
+    protected String address;
+
+    /**
+     * Obtém o valor da propriedade name.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Define o valor da propriedade name.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Obtém o valor da propriedade standard.
+     * 
+     */
+    public int getStandard() {
+        return standard;
+    }
+
+    /**
+     * Define o valor da propriedade standard.
+     * 
+     */
+    public void setStandard(int value) {
+        this.standard = value;
+    }
+
+    /**
+     * Obtém o valor da propriedade address.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Define o valor da propriedade address.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAddress(String value) {
+        this.address = value;
+    }
+
+}
